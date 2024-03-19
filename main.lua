10f8c31b9b255435660f3e8a924d2770ef0b7914
sdkv2 <aidenshihadeh@gmail.com>
2024-03-19 16:51:13 +0000

    dd

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 function love.load()
     y = 1
     x = 0
+    r = 0
     downwards = false
     font = love.graphics.newFont("papyrus.ttf", 64)
     myImage = love.graphics.newImage("sheep.png")
@@ -13,32 +14,41 @@ end
 
 function createSheep()
     sheep = {}
-    sheep.x = love.math.random(0, 800)
+    sheep.x = x + 120
     sheep.y = love.math.random(0, 100)
-    sheep.speed = 100
+    sheep.speed = love.math.random(0.5, 10)
     sheep.image = love.graphics.newImage("sheep.png")
     sheep.scale = love.math.random(1, 3)
     sheep.red = love.math.random(0, 255)
     sheep.green = love.math.random(0, 255)
     sheep.blue = love.math.random(0, 255)
+    sheep.downwards = false
+    sheep.x_shear = love.math.random(-1.0, 1.0)
+    sheep.y_shear = love.math.random(-1.0, 1.0)
+
 
     table.insert(sheeps, sheep)
     return sheep
 end
+
+function updateY(y, dt, downwards, speed)
+    if y >= 600 or downwards == true then
+        y = y - 90 * dt * speed
+        downwards = true
+
+        if y <= 0 then
+            downwards = false
+        end
+
+    elseif downwards == false or y <= 0 then
+        downwards = false
+        y = y + 100 * dt * speed
+    end
+
+    return y, downwards
+end
  
 function love.update(dt)
-        if y >= 600 or downwards == true then
-            y = y - 90 * dt
-            downwards = true
-
-            if y <= 0 then
-                downwards = false
-            end
-
-        elseif downwards == false or y <= 0 then
-            downwards = false
-            y = y + 100 * dt
-        end
     if love.keyboard.isDown("right") and x < 800 then
         x = x + 3
     end
@@ -46,6 +56,10 @@ function love.update(dt)
         x = x - 3
     end
     
+    for i,v in ipairs(sheeps) do
+        v.y, v.downwards = updateY(v.y, dt, v.downwards, v.speed)
+    end
+    r = r + 0.01
 end
 
 function love.keypressed(key)
@@ -57,7 +71,7 @@ end
 function love.draw()
     for i,v in ipairs(sheeps) do
         love.graphics.setColor(v.red/255, v.green/255, v.blue/255)
-        love.graphics.draw(v.image, v.x, v.y + y, 0, v.scale, v.scale)
+        love.graphics.draw(v.image, v.x, v.y, r, v.scale, v.scale, nil, nil, v.x_shear, v.y_shear)
     end
     love.graphics.setColor(1, 1, 1)
     love.graphics.draw(myText, 100 + x, y + 75)
